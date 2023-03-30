7a83be98597c5365cc58b9efe9bf745a542febc5
liaozhongwei <1685268582@qq.com>
2023-03-30 23:22:13 +0800

    add suanfa

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -5,7 +5,10 @@ import com.example.demo.mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import java.util.ArrayList;
+import static com.example.demo.fun.HandleController.handleUser_test1;
+import static com.example.demo.fun.HandleController.handleUser_test3;
+
 
 @RestController
 @RequestMapping("/users")
@@ -15,20 +18,59 @@ public class UserController {
     private UserMapper userMapper;
 
     @GetMapping("")
-    public List<User> getAllUsers() {
+    public ArrayList<User> getAllUsers(@RequestParam("agemax") int agemax, @RequestParam("target") int target) {
         System.out.println("查询全表");
-        return userMapper.getAllUsers();
+        ArrayList<User> users =  userMapper.getAllUsers();
+        /**
+         * test1. 参数：搜索名称含有明的用户们
+         * test2. 年龄 < 25
+         * test3. 找出两个年纪之和为50的人员
+         *
+         * */
+//        List<User> res = null;
+//        for (User u: users) {
+//            if (u.name.contains('明'))  res.add(u);
+//        }
+        for (User user : users) {
+            System.out.println("姓名: "+user.getName());
+            System.out.println("年龄: "+user.getAge());
+            System.out.println("id:   "+user.getId());
+            System.out.println("邮箱: "+user.getEmail());
+        }
+        users = handleUser_test3(users, target);
+
+        System.out.println("+++++++++处理数据后+++++++++");
+        for (User user : users) {
+            System.out.println("姓名: "+user.getName());
+            System.out.println("年龄: "+user.getAge());
+            System.out.println("id:   "+user.getId());
+            System.out.println("邮箱: "+user.getEmail());
+        }
+        return users;
     }
 
     @GetMapping("/{id}")
+    // 路径参数（变量）
+    // 接收参数
     public User getUserById(@PathVariable int id) {
         System.out.println("根据id查询数据");
         return userMapper.getUserById(id);
     }
 
-    @PostMapping("")
-    public String insertUser(@RequestBody User user) {
+    @PostMapping("/test")
+    public String insertTestUser(@RequestParam("name") String name, @RequestParam("age") int age, @RequestParam("email") String email, @RequestBody User user) {
         System.out.println("插入一条用户数据");
+
+        System.out.println(user);
+
+        userMapper.insertUser(user);
+        return "insert user success";
+    }
+    @PostMapping("")
+    public String insertUser(@RequestBody User user){
+        System.out.println("插入一条用户数据");
+        System.out.println(user);
+
         userMapper.insertUser(user);
         return "insert user success";
     }
--- src/main/java/com/example/demo/mapper/UserMapper.java
+++ src/main/java/com/example/demo/mapper/UserMapper.java
@@ -1,14 +1,15 @@
 package com.example.demo.mapper;
 
-import java.util.List;
+import java.util.ArrayList;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import com.example.demo.entity.User;
-import org.springframework.stereotype.Repository;
+
 
 @Mapper
+
 public interface UserMapper {
-    List<User> getAllUsers();
+    ArrayList<User> getAllUsers();
     User getUserById(@Param("id") int id);
     void insertUser(User user);
     void updateUser(User user);
